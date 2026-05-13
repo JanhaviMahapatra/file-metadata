28d9de906941c6dd4a5c55bf6a9ecb216a921c46
janhavi015 <janhaviss005gmail.com>
2026-05-13 15:37:55 +0530

    completed file metadata microservice

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+// multer setup
+const upload = multer({
+  dest: 'uploads/'
 });
+
+
+// file upload route
+app.post(
+  '/api/fileanalyse',
+  upload.single('upfile'),
+  function(req, res) {
+
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+
+  }
+);
+
+
+// server
+const listener = app.listen(
+  process.env.PORT || 3000,
+  function() {
+
+    console.log(
+      'Your app is listening on port ' +
+      listener.address().port
+    );
+
+  }
+);
