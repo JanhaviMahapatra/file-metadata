22948f49fe4e567689cd13d1655cfb089afdd45b
janhavi015 <janhaviss005gmail.com>
2026-05-14 15:32:06 +0530

    fix railway port

--- index.js
+++ index.js
@@ -34,6 +34,3 @@ app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
 
 });
 
-const listener = app.listen(process.env.PORT || 3000, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
